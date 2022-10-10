80de5b1aeb80e06332505e5b82dfa5b15a73fb23
Albin <albin.coding@gmail.com>
2022-10-10 16:46:58 +1300

    Added the basic game functionality
    Added a function which checks who wins in the round.

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ function getComputerChoice(){
     return computerSelection;
 }
 
+function playRound(computerSelection, playerSelection){
+    if (computerSelection == playerSelection) {
+        return 'It is a drawww'
+    } else if (computerSelection == 'rock' && playerSelection == 'scissors') {
+        return 'Computer Wins!! Rock beats scissors'
+    } else if (computerSelection == 'rock' && playerSelection == 'paper') {
+        return 'Player Wins!! Paper beats Rock!'
+    } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
+        return "Computer Wins!! Scissors beats paper"
+    } else if (computerSelection == 'scissors' && playerSelection == 'rock') {
+        return "Player Wins!! Rock beats Scissors!"
+    } else if (computerSelection == 'paper' && playerSelection == 'rock') {
+        return "Computer Wins!! Paper beats rock"
+    } else if (computerSelection == 'paper' && playerSelection == 'scissors'){
+        return "Player Wins!! Scissors beats paper!"
+    }
+}
 
-
-console.log(getComputerChoice())
+console.log(playRound(getComputerChoice(), playerSelection))
